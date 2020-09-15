440e8756eed9ef67ec4bd0ad7cd249a059bbc7f0
Bryce <bsaito88@gmail.com>
2020-09-14 16:33:54 -1000

    Removed payload.

--- app/store/actions/healthcareAuthorities/autoSelectHdohAction.ts
+++ app/store/actions/healthcareAuthorities/autoSelectHdohAction.ts
@@ -1,18 +1,9 @@
 import { createAction } from '@reduxjs/toolkit';
-import type { HealthcareAuthority } from '../../types';
-
-type Payload = {
-  healthcareAuthorities: HealthcareAuthority[];
-  // No custom URLs like toggleSelect action
-};
 
 const AUTO_SELECT_HDOH = 'AUTO_SELECT_HDOH';
 
 const autoSelectHdohAction = createAction(
   AUTO_SELECT_HDOH,
-  ({ healthcareAuthorities }: Payload) => ({
-    payload: { healthcareAuthorities },
-  }),
 );
 
 export default autoSelectHdohAction;
